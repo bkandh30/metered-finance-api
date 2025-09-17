6def3319d0b2452ef23da4a8f476443b6a19fcb6
bkandh30 <kandhari.bhavya20@gmail.com>
2025-09-16 21:15:31 -0700

    Updated sqlx migration down file for cleanup function

--- migrations/20250917023046_add_cleanup_function.down.sql
+++ migrations/20250917023046_add_cleanup_function.down.sql
@@ -1,4 +1,4 @@
 -- Add down migration script here
-DROP INDEX IF EXISTS idx_idempotency_cleanup;
+DROP INDEX IF EXISTS idx_idempotency_expires;
 
 DROP FUNCTION IF EXISTS cleanup_expired_idempotency_keys();
